520b130bc565744f4b6fb6846675bf8b7fa3601f
marzu <marzuqazam06@gmail.com>
2024-10-17 12:53:06 -0400

    lab 08

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
+
+
 public class SafeInput {
+
+    {
+
+        Scanner in = new Scanner(System.in);
+        String firstName = "";
+
+        firstName = getNonZeroLenString(in, "Enter your first name");
+
+        System.out.println("Fname is " + firstName);
+    }
+
+    /**
+     * gets a string from the user using the console
+     * It must be at least one character in length
+     * @param pipe a Scanner used to get the input
+     * @param prompt the prompt that tells the user what to enter
+     * @return A string of at least one character in length
+     */
+
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retVal = "";
+
+        do {
+            System.out.print(prompt + ": ");
+            retVal = pipe.nextLine();
+
+            if(retVal.isEmpty())
+                System.out.println("must enter at least one character.");
+
+
+        } while(retVal.isEmpty());
+
+        return retVal;
+    }
 }
