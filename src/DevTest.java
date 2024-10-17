3c257748141fd7280c2a9ab6147f3d5720dcb041
marzu <marzuqazam06@gmail.com>
2024-10-17 13:15:13 -0400

    lab 08

--- src/DevTest.java
+++ src/DevTest.java
@@ -6,10 +6,19 @@ public class DevTest
     {
         Scanner in = new Scanner(System.in);
         String firstName = "";
-
+        int age = 0;
+        double salary = 0;
+/*
         firstName = getNonZeroLenString(in, "Enter your first name");
 
         System.out.println("Fname is " + firstName);
+
+
+        age = getInt(in, "Enter your age");
+        System.out.println("you said your age is " + age);
+*/
+        salary = getDouble(in, "enter your salary");
+        System.out.println("Salary is " + salary);
     }
 
     public static String getNonZeroLenString(Scanner pipe, String prompt)
@@ -28,4 +37,74 @@ public class DevTest
 
         return retVal;
     }
+
+    /**
+     * gets an int value from the user at the console with no constraint
+     * @param pipe a Scanner used to get the input
+     * @param prompt the prompt that tells the user what to enter
+     * @return an int of any value
+     */
+
+
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        int retVal = 0;
+        boolean done = false;
+        String trash = "";
+
+        do
+        {
+            System.out.print(prompt + ": ");
+            if(pipe.hasNextInt())
+            {
+                retVal = pipe.nextInt();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("you must enter a valid integer and not " + trash);
+            }
+
+        } while(!done);
+
+        return retVal;
+    }
+
+
+    /**
+     * gets a double value from the user at the console with no constraint
+     * @param pipe a Scanner used to get the input
+     * @param prompt the prompt that tells the user what to enter
+     * @return an int of any value
+     */
+
+
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+        double retVal = 0;
+        boolean done = false;
+        String trash = "";
+
+        do
+        {
+            System.out.print(prompt + ": ");
+            if(pipe.hasNextDouble())
+            {
+                retVal = pipe.nextDouble();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("you must enter a valid double and not " + trash);
+            }
+
+        } while(!done);
+
+        return retVal;
+    }
+
 }
