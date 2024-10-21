0f6c6a050c3e780706797a97b50979539fee012a
marzu <marzuqazam06@gmail.com>
2024-10-21 17:16:34 -0400

    lab 08

--- src/DevTest.java
+++ src/DevTest.java
@@ -9,6 +9,7 @@ public class DevTest
         int age = 0;
         int favNum = 0;
         double salary = 0;
+        double constrained = 0; // must be between 100 and 1000
 
 /*
         firstName = getNonZeroLenString(in, "Enter your first name");
@@ -21,9 +22,11 @@ public class DevTest
 
         salary = getDouble(in, "enter your salary");
         System.out.println("Salary is " + salary);
-*/
+
         favNum = getRangedInt(in, "enter your favorite number", 1, 10);
         System.out.println("Fav num is " + favNum);
+ */
+        constrained = getRangedDouble(in, "Enter your constrained double value", 100, 1000);
     }
 
     public static String getNonZeroLenString(Scanner pipe, String prompt)
@@ -150,6 +153,96 @@ public class DevTest
             {
                 trash = pipe.nextLine();
                 System.out.println("you must enter a valid integer and not " + trash);
+            }
+
+        } while(!done);
+
+        return retVal;
+    }
+
+
+
+    /**
+     * gets a double from the user via the console within a specified range
+     *
+     * @param pipe the scanner to use for input
+     * @param prompt the prompt to tell the user what is required
+     * @param low the inclusive low bound
+     * @param high the inclusive high bound
+     * @return a double within the specified range
+     */
+
+
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high)
+    {
+        double retVal = 0;
+        boolean done = false;
+        String trash = "";
+
+        do
+        {
+            System.out.print(prompt + "[" + low + " - " + high + "]: ");
+            if(pipe.hasNextDouble())
+            {
+                retVal = pipe.nextDouble();
+                pipe.nextLine();
+                if(retVal >= low && retVal <= high)
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("you must enter a value within the range [" + low + " - " + high + "]: ");
+                }
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("you must enter a valid double and not " + trash);
+            }
+
+        } while(!done);
+
+        return retVal;
+    }
+
+    /**
+     * Gets a Yes or No from the user and returns the equivalent true or false
+     *
+     * @param pipe scanner to use for input
+     * @param prompt tells the user what to enter
+     * @return true or false
+     */
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt)
+    {
+        String YNResponse = "";
+        boolean retVal = false;
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + ": ");
+            YNResponse = pipe.nextLine();
+
+            if(YNResponse.matches("[YyNn]"))
+            {
+                System.out.println("must enter [Y/N]: ");
+
+            }
+            else
+            {
+                done = true;
+                switch (YNResponse)
+                {
+                    case "Y":
+                    case "y":
+                        retVal = true;
+                        break;
+                    case "N":
+                    case "n":
+                        retVal = false;
+                        break;
+                }
             }
 
         } while(!done);
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -152,4 +152,94 @@ public class SafeInput {
 
         return retVal;
     }
+
+
+    /**
+     * gets a double from the user via the console within a specified range
+     *
+     * @param pipe the scanner to use for input
+     * @param prompt the prompt to tell the user what is required
+     * @param low the inclusive low bound
+     * @param high the inclusive high bound
+     * @return a double within the specified range
+     */
+
+
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high)
+    {
+        double retVal = 0;
+        boolean done = false;
+        String trash = "";
+
+        do
+        {
+            System.out.print(prompt + "[" + low + " - " + high + "]: ");
+            if(pipe.hasNextDouble())
+            {
+                retVal = pipe.nextDouble();
+                pipe.nextLine();
+                if(retVal >= low && retVal <= high)
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("you must enter a value within the range [" + low + " - " + high + "]: ");
+                }
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("you must enter a valid double and not " + trash);
+            }
+
+        } while(!done);
+
+        return retVal;
+    }
+
+
+    /**
+     * Gets a Yes or No from the user and returns the equivalent true or false
+     *
+     * @param pipe scanner to use for input
+     * @param prompt tells the user what to enter
+     * @return true or false
+     */
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt)
+    {
+        String YNResponse = "";
+        boolean retVal = false;
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + ": ");
+            YNResponse = pipe.nextLine();
+
+            if(YNResponse.matches("[YyNn]"))
+            {
+                System.out.println("must enter [Y/N]: ");
+
+            }
+            else
+            {
+                done = true;
+                switch (YNResponse)
+                {
+                    case "Y":
+                    case "y":
+                        retVal = true;
+                        break;
+                    case "N":
+                    case "n":
+                        retVal = false;
+                        break;
+                }
+            }
+
+        } while(!done);
+
+        return retVal;
+    }
 }
